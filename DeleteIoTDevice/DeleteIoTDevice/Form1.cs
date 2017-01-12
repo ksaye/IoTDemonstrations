d76ea1fc86d34d0e5cffa8d1aca33c1f965258d7
ksaye <kevinsay@microsoft.com>
2017-01-11 23:37:52 -0600

    initial relase

---
+++ DeleteIoTDevice/DeleteIoTDevice/Form1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeleteIoTDevice
+{
+
+    public partial class Form1 : Form
+    {
+        RegistryManager registryManager;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                registryManager = RegistryManager.CreateFromConnectionString(connectionString.Text);
+                await registryManager.RemoveDeviceAsync(deviceId.Text);
+                MessageBox.Show("Success: " + deviceId.Text + " deleted.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("Error: " + er.Message, null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            connectionString.Text = "HostName=test2.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=MuO8hHlWt15t1zEm1Y2th/JbFxp48vAHNDzDWZZfpNk=";
+        }
+
+    }
+}
