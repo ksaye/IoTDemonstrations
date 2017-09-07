eeb6aa4ac634ee1d21e77eed66c584c3be5172c5
ksaye <ksaye@saye.org>
2017-09-07 15:26:07 -0500

    initial upload

---
+++ HotTub/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Android.OS;
+
+namespace MyHotTub
+{
+    [Activity(Label = "My Hot Tub", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var webView = FindViewById<WebView>(Resource.Id.webView1);
+            webView.SetWebViewClient(new WebViewClient());
+            webView.Settings.JavaScriptEnabled = true;
+            webView.LoadUrl("http://hottubweb.azurewebsites.net/");
+
+        }
+
+    }
+}
+
