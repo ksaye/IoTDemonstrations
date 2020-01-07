e55e5b69e1b0262ccfcbd66db665c6e659081f3b
ksaye <ksaye@saye.org>
2020-01-07 17:32:40 -0600

    Update run.sh

--- batch/run.sh
+++ batch/run.sh
@@ -1,7 +1,6 @@
 # setup on Standard D64 v3 (64 vcpus, 256 GiB memory)
 sudo apt update
 sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev xterm locales
-sudo dpkg-reconfigure locales
 
 USERGROUP=`id -gn`
 BUILD=warrior
@@ -14,6 +13,9 @@ sudo chown -R $USER:$USERGROUP /mnt/yocto
 cd /mnt/yocto/source
 rm -f -r *
 sudo locale-gen en_US.UTF-8
+LC_ALL=en_US.UTF-8
+LANG=en_US.UTF-8
+LANGUAGE=en_US.UTF-8
 
 git clone -b $BUILD http://git.yoctoproject.org/git/poky
 git clone -b $BUILD http://git.yoctoproject.org/git/meta-intel
