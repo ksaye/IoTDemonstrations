a99f3877349a4ddca7c0473b8bc3ede7a52ef8ce
ksaye <ksaye@saye.org>
2020-01-07 17:27:39 -0600

    Update run.sh

--- batch/run.sh
+++ batch/run.sh
@@ -1,6 +1,7 @@
 # setup on Standard D64 v3 (64 vcpus, 256 GiB memory)
 sudo apt update
 sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev xterm locales
+sudo dpkg-reconfigure locales
 
 USERGROUP=`id -gn`
 BUILD=warrior
