b716647827331d21d6af80386f2c4f17ddfe5fcc
ksaye <ksaye@saye.org>
2020-01-07 21:10:46 -0600

    Update run.sh

--- batch/run.sh
+++ batch/run.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt-get install -y gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat cpio python python3 python3-pip python3-pexpect xz-utils debianutils iputils-ping python3-git python3-jinja2 libegl1-mesa libsdl1.2-dev xterm locales
 
 USERGROUP=`id -gn`
-BUILD=thud
+BUILD=warrior
 MACHINETYPE=intel-corei7-64
 
 sudo rm -f -r /mnt/yocto
