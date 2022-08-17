674707b9575421bb0523ee6ff86b43120e83a24d
ksaye <ksaye@saye.org>
2022-08-17 12:04:03 -0500

    Create kub.sh

---
+++ kub.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+sudo swapoff -a && sudo sed -i -e '/swap.img/d' /etc/fstab
+sudo sudo modprobe br_netfilter && sudo sysctl -w net.bridge.bridge-nf-call-iptables=1 && sudo sysctl -w net.ipv4.ip_forward=1
+sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl containerd
+
+# kubernetes time
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubectl && sudo apt-mark hold kubelet kubeadm kubectl
+sudo sed -i "s/cgroupDriver: systemd/cgroupDriver: cgroupfs/g" /var/lib/kubelet/config.yaml
+sudo systemctl daemon-reload && sudo systemctl restart kubelet
+kubectl version --output=json
