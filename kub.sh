9a9db8da5660af693a1dc519449196f57b6b68d5
ksaye <ksaye@saye.org>
2022-08-17 16:08:57 -0500

    Update kub.sh

--- kub.sh
+++ kub.sh
@@ -1,11 +1,35 @@
 #!/usr/bin/env bash
 
 sudo swapoff -a && sudo sed -i -e '/swap.img/d' /etc/fstab
-sudo sudo modprobe br_netfilter && sudo sysctl -w net.bridge.bridge-nf-call-iptables=1 && sudo sysctl -w net.ipv4.ip_forward=1
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl containerd
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+
+sudo sysctl --system
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update -y 
+sudo apt install -y containerd.io
+
+sudo mkdir -p /etc/containerd
+sudo containerd config default | sudo tee /etc/containerd/config.toml
+sudo sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
 
 # kubernetes time
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubectl && sudo apt-mark hold kubelet kubeadm kubectl
 kubectl version --output=json
+sudo systemctl enable kubelet
