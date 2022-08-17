a0c57c6cd672ea41ba4fb85c218a4c0619711b04
ksaye <ksaye@saye.org>
2022-08-17 12:40:20 -0500

    Update kub.sh

--- kub.sh
+++ kub.sh
@@ -8,6 +8,4 @@ sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificat
 sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubectl && sudo apt-mark hold kubelet kubeadm kubectl
-sudo sed -i "s/cgroupDriver: systemd/cgroupDriver: cgroupfs/g" /var/lib/kubelet/config.yaml
-sudo systemctl daemon-reload && sudo systemctl restart kubelet
 kubectl version --output=json
